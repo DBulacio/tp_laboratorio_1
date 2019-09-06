dcc3e2bc18789e6d046a828f807bae7d4e477547
DBulacio <44949670+DBulacio@users.noreply.github.com>
2019-09-06 14:57:10 -0300

    ULTIMOS CAMBIOS

--- tp_1/main.c
+++ tp_1/main.c
@@ -10,7 +10,7 @@ int main()
     int num1 = 0, num2 = 0;
     int rSum, rRes, rMult, rFactA, rFactB;
     float rDiv;
-    int flag1 = 0, flag2 = 0, fSuma = 0, fResta = 0, fMult = 0, fDiv = 0, fFact = 0;
+    int flag1 = 0, flag2 = 0;
 
     do {
         switch(menu(num1, num2, flag1, flag2)){
@@ -26,99 +26,36 @@ int main()
                 break;
             case 3:
                 if(flag1 == 1 && flag2 == 1){
-                    printf("Seleccione la operacion a realizar: ");
-                    fflush(stdin);
-                    char option = getchar();
-                    //menu de operaciones
-                    switch(option){
-                        case 'a':
-                            rSum = sumar(num1, num2);
-                            fSuma = 1;
-                            break;
-                        case 'b':
-                            rRes = restar(num1, num2);
-                            fResta = 1;
-                            break;
-                        case 'c':
-                            // si hacen la division con b = 0, forzamos a que lo cambie
-                            if(num2 != 0){
-                                rDiv = dividir(num1, num2);
-                                fDiv = 1;
-                            } else {
-                                printf("Ingrese un valor distinto a cero para B\n");
-                            }
-                            break;
-                        case 'd':
-                            rMult = multiplicar(num1, num2);
-                            fMult = 1;
-                            break;
-                        case 'e':
-                            rFactA = factorialA(num1);
-                            rFactB = factorialB(num2);
-                            fFact = 1;
-                            break;
-                        default:
-                            printf("ERROR: elija una operacion valida\n");
-                            break;
-                    }
+                    printf("CALCULANDO...\n");
+                    rSum = sumar(num1, num2);
+                    rRes = restar(num1, num2);
+                    rDiv = dividir(num1, num2);
+                    rMult = multiplicar(num1, num2);
+                    rFactA = factorialA(num1);
+                    rFactB = factorialB(num2);
                 } else {
                     printf("Ingrese el operando faltante\n");
                 }
                 break;
             case 4:
-                printf("Seleccione el resultado a mostrar: ");
-                fflush(stdin);
-                char option = getchar();
-                // menu de resultados
-                switch(option){
-                    case 'a':
-                        if(fSuma == 1){
-                           printf("El resultado de la suma es: %d\n", rSum);
-                        } else {
-                            printf("Tiene que realizar la suma para ver el resultado.\n");
-                        }
-                        break;
-                    case 'b':
-                        if(fResta == 1){
-                            printf("El resultado de la resta es: %d\n", rRes);
-                        } else {
-                            printf("Tiene que realizar la resta para ver el resultado.\n");
-                        }
-                        break;
-                    case 'c':
-                        if(fDiv == 1){
-                            printf("El resultado de la division es: %f\n", rDiv);
-                        } else {
-                            printf("Tiene que realizar la division para ver el resultado.\n");
-                        }
-                        break;
-                    case 'd':
-                        if(fMult == 1){
-                            printf("El resultado de la multiplicacion es: %d\n", rMult);
-                        } else {
-                            printf("Tiene que realizar la multiplicacion para ver el resultado.\n");
-                        }
-                        break;
-                    case 'e':
-                        if(fFact == 1){
-                            // Si se hizo la operacion, damos un mensaje para cada resultado posible
-                            if(rFactA == 0 && rFactB > 0){
-                                printf("El factorial de A no se puede calcular y el factorial de B es: %d\n", rFactB);
-                            } else if(rFactA > 0 && rFactB == 0){
-                                printf("El factorial de A es %d y el factorial de B no se puede calcular\n", rFactA);
-                            } else if(rFactA == 0 && rFactB == 0){
-                                printf("No se puede calcular el factorial de A ni de B\n");
-                            } else {
-                            printf("El factorial de A es: %d y El factorial de B es: %d\n", rFactA, rFactB);
-                            }
-                        } else {
-                            printf("Tiene que realizar la factorizacion para ver el resultado.\n");
-                        }
-                        break;
-                    default:
-                        printf("ERROR: elija una opcion valida\n");
-                        break;
-                    }
+                printf("El resultado de la suma es: %d\n", rSum);
+                printf("El resultado de la resta es: %d\n", rRes);
+                if(num2 != 0){
+                    printf("El resultado de la division es: %f\n", rDiv);
+                } else {
+                    printf("No se puedo realizar la division porque el dividendo es 0");
+                }
+                printf("El resultado de la multiplicacion es: %d\n", rMult);
+                // Si se hizo la operacion, damos un mensaje para cada resultado posible
+                if(rFactA == 0 && rFactB > 0){
+                    printf("El factorial de A no se puede calcular y el factorial de B es: %d\n", rFactB);
+                } else if(rFactA > 0 && rFactB == 0){
+                    printf("El factorial de A es %d y el factorial de B no se puede calcular\n", rFactA);
+                } else if(rFactA == 0 && rFactB == 0){
+                    printf("No se puede calcular el factorial de A ni de B\n");
+                } else {
+                printf("El factorial de A es: %d y El factorial de B es: %d\n", rFactA, rFactB);
+                }
                 break;
             case 5:
                 printf("¿Seguro quiere salir? (s/n) ");
